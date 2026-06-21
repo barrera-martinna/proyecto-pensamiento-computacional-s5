d85bac3a8e7a3c4e101d0beb5c5276390eda4535
barrera-martinna <martinna.barrera@mail.udp.cl>
2026-06-21 17:16:43 -0400

    up pensar

--- avance-examen.js
+++ avance-examen.js
@@ -1,1 +1,4 @@
 //21 de junio del 2026
+//escenas con referencia a la pelicula de up me gustaria que la casa aparezca,luego le vayan saliendo los globos y despues como que suba despues de llegar a su limite de alto esta comience a caer de manera suave
+// escena 1 quiero que aparezca la casa de up y al hacer click izquierdo en el mouse vayan apareciendo los globos sobre la casita de up
+// escena 2 que este la casita con los globos y que al hacer scroll la casita subir al cielo y flotar
